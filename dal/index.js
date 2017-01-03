2515315a3c30fec27d9a5cde3331fcf8e4b1a1e4
samhwang1990 <samhwang1990@gmail.com>
2017-01-03 08:47:01 +0800

    UserInfo Module ing

---
+++ dal/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by sam on 17/1/3.
+ */
+
+exports.UserInfo = require('./user_info');
+exports.UserPassword = require('./user_password');
+exports.GroupInfo = require('./group_info');
+exports.PlanBox = require('./plan_box');
---
+++ modules/UserInfo/index.js
@@ -0,0 +1,24 @@
+/**
+ * Created by sam on 17/1/3.
+ */
+
+const Dal = require('../../dal/index');
+const UserInfoDal = Dal.UserInfo;
+
+exports.createUser = function createUser(userInfo) {
+  return new Promise((resolve, reject) => {
+
+  });
+};
+
+exports.updateUser = function updateUser() {
+
+};
+
+exports.deleteUser = function deleteUser() {
+
+};
+
+exports.getUser = function getUser() {
+
+};
